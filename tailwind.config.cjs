3e5846df85187e2be649594d62608aaac2600725
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2023-12-20 20:05:33 +0100

    start work on landing page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,6 +14,7 @@ const config = {
 		  colors: {
 			primary: 'rgb(var(--color-primary) / <alpha-value>)',
 			secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+			tertiary: 'rgb(var(--color-tertiary) / <alpha-value>)',
 		  }
 		}
 	  },
